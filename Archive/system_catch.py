c896a1a6a37b765b29eeb5deaeced355126aa0ba
Bing Wang <toaya.kase@gmail.com>
2012-05-27 13:51:46 +0800

    rm some rubbish

--- Archive/system_catch.py
+++
@@ -1,7 +0,0 @@
-import os
-g = open("system_data","w")
-for l in os.listdir("/Users/bingwang/VimWork/BioNetWork/Archive/"):
-    if l.endswith(".tab"):
-        f = open(l)
-        g.write(f.read())
-
--- Genetic_Cross/rename.py
+++
@@ -1,8 +0,0 @@
-import os
-path = "/Users/bingwang/VimWork/BioNetWork/Genetic_Length/"
-for fi in os.listdir(path):
-    if fi.endswith(".png"):
-        if len(fi) == 6:
-            os.system("mv "+path+fi+" "+path+"0"+fi)
-        if len(fi) == 5:
-            os.system("mv "+path+fi+" "+path+"00"+fi)
